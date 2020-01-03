ba950f86ccee6fe20c8aa9e8a330f4df84bb599a
Flavio F Galanti <flavio@ds-info.com>
2020-01-03 15:59:09 -0300

    FORM para ADD paciente, adicionando paciente usando JavaScript

--- js/principal.js
+++ js/principal.js
@@ -3,8 +3,6 @@ titulo.textContent = 'Aparecida Nutricionista';
 
 var pacientes = document.querySelectorAll('.paciente');
 
-console.log(pacientes.length);
-
 for(var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
 
@@ -34,3 +32,47 @@ for(var i = 0; i < pacientes.length; i++) {
         paciente.classList.add('paciente-invalido');
     }
 }
+
+var btnAdicionaPaciente = document.querySelector('#adicionar-paciente');
+btnAdicionaPaciente.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    var form = document.querySelector('#form-adiciona');
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add('paciente');
+
+    var nomeTd = document.createElement("td");
+    nomeTd.classList.add('info-nome');
+
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add('info-peso');
+
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add('info-altura');
+
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add('info-gordura');
+
+    var imcTd = document.createElement("td");
+    imcTd.classList.add('info-imc');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+    
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
