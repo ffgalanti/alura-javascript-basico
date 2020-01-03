a98538f60092ab9026317cb460840eea81402915
Flavio F Galanti <flavio@ds-info.com>
2020-01-03 11:42:01 -0300

    usando IF, operador OR(||) e AND(&&) no JavaScript

--- js/principal.js
+++ js/principal.js
@@ -8,6 +8,21 @@ var tdImc = paciente.querySelector('.info-imc');
 
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
-var imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+var pesoHeValido = true;
+var alturaHeValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoHeValido = false;
+}
+
+if (altura <= 0 || altura >= 3.0) {
+    alturaHeValida = false;
+}
+
+if(pesoHeValido && alturaHeValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = '----';
+}
