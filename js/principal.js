dd75dc6bcc61727f30ba28f2bbccaa60656ff204
Flavio F Galanti <flavio@ds-info.com>
2020-01-03 12:07:38 -0300

    usando FOR para percorrer todos os pacientes no JavaScript

--- js/principal.js
+++ js/principal.js
@@ -1,28 +1,34 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
 
-var paciente = document.querySelector('#primeiro-paciente');
-var tdPeso = paciente.querySelector('.info-peso');
-var tdAltura = paciente.querySelector('.info-altura');
-var tdImc = paciente.querySelector('.info-imc');
+var pacientes = document.querySelectorAll('.paciente');
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+console.log(pacientes.length);
 
-var pesoHeValido = true;
-var alturaHeValida = true;
+for(var i = 0; i < pacientes.length; i++) {
 
-if (peso <= 0 || peso >= 1000) {
-    pesoHeValido = false;
-}
-
-if (altura <= 0 || altura >= 3.0) {
-    alturaHeValida = false;
-}
-
-if(pesoHeValido && alturaHeValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-} else {
-    tdImc.textContent = '----';
+    var tdPeso = pacientes[i].querySelector('.info-peso');
+    var tdAltura = pacientes[i].querySelector('.info-altura');
+    var tdImc = pacientes[i].querySelector('.info-imc');
+    
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    if (peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+    }
+    
+    if (altura <= 0 || altura >= 3.0) {
+        alturaEhValida = false;
+    }
+    
+    if(pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = 'Peso/Altura inválido';
+    }
 }
